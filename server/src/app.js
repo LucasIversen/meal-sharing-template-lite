2b95a0e49e8bd2eb5d8c51addb4441c934db90fd
Lucas Iversen <43264353+LucasIversen@users.noreply.github.com>
2023-05-17 22:16:29 +0200

    Revert "Merge pull request #1 from MercedesUbeira/lucas_update"
    This reverts commit d498390d41ebb53356af47c2a58ea344d94d1bd8, reversing
    changes made to 88fc3ec31d3be1a04a8050a41858bcbeb4055b17.

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 const mealsRouter = require("./api/meals");
 const cors = require("cors");
-const knex = require("./database");
 require("dotenv").config(); // Load environment variables from .env file
 const port = process.env.PORT || 3000;
 
@@ -15,69 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-// Add routes, starting with /meals
-router.use("/meals", mealsRouter);
-
 // Define routes for your app
-app.get("/future-meals", async (req, res) => {
-  const now = new Date();
-  try {
-    const query = knex.from("Meal").select("when").where("when", ">", now);
-    const results = await query;
-    res.json(results);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" }); // Add your code here
 });
 
-app.get("/past-meals", async (req, res) => {
-  const now = new Date();
-  try {
-    const query = knex.from("Meal").select("when").where("when", "<", now);
-    const results = await query;
-    res.json(results);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.get("/all-meals", async (req, res) => {
-  try {
-    const query = knex.from("Meal").select("id").orderBy("id", "asc");
-    const results = await query;
-    res.json(results);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.get("/first-meal", async (req, res) => {
-  try {
-    const query = knex.from("Meal").select("id").orderBy("id", "asc").first();
-    const results = await query;
-    if (results) {
-      res.json(results);
-    } else {
-      res.status(404).send("Couldn't get first-meal");
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.get("/last-meal", async (req, res) => {
-  try {
-    const query = knex.from("Meal").select("id").orderBy("id", "desc").first();
-    const results = await query;
-    if (results) {
-      res.json(results);
-    } else {
-      res.status(404).send("Couldn't get last-meal");
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+router.use("/meals", mealsRouter);
 
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
